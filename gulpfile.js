8312053b0f909b1a66ce69568f89046074bd2b2b
beserker1 <aaryanbhagat377@gmail.com>
2019-05-30 12:07:46 +0530

    modified a lot

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var messages = {
 };
 
 var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
-
+console.log(browserSync);
 /**
  * Build the Jekyll Site
  */
@@ -30,100 +30,59 @@ gulp.task('jekyll-build', function (done) {
  * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', gulp.series('jekyll-build'), function () {
-	try{
-		console.log('jekyll-rebuild started');
-		browserSync.reload();
-	}
-	catch(e)
-	{
-		console.log("Oh snap jekyll rebuild failed");
-		console.log(e);
-	}
-	return Promise.resolve('done');
+	browserSync.reload();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
 gulp.task('browser-sync', gulp.series('jekyll-build'), function() {
-	try{
-		console.log('browser-sync started');
-		browserSync({
-			server: {
-				baseDir: '_site'
-			}
-		});
-		console.log('browser-sync completed hooray!');
-	}
-	catch(e){
-		console.log('browser-sync failed');
-		console.log(e);
-	}
-	return Promise.resolve('done');
+	browserSync.init({
+		server: {
+			baseDir: '_site'
+		}
+	});
 });
 
 /**
  * Stylus task
  */
-gulp.task('stylus', function(){
-		try{
-			console.log('Stylus started');
-			gulp.src('src/styl/main.styl')
-			.pipe(plumber())
-			.pipe(stylus({
-				use:[koutoSwiss(), prefixer(), jeet(),rupture()],
-				compress: true
-			}))
-			.pipe(gulp.dest('_site/assets/css/'))
-			.pipe(browserSync.reload({stream:true}))
-			.pipe(gulp.dest('assets/css'));
-			console.log('Stylus done');
-		}
-		catch(e){
-			console.log('Stylus failed');
-			console.log(e);
-		}
-		return Promise.resolve('Done');
+gulp.task('stylus', function(cb){
+	gulp.src('src/styl/main.styl')
+	.pipe(plumber())
+	.pipe(stylus({
+		use:[koutoSwiss(), prefixer(), jeet(),rupture()],
+		compress: true
+	}))
+	.pipe(gulp.dest('_site/assets/css/'))
+	.pipe(browserSync.reload({stream:true}))
+	.pipe(gulp.dest('assets/css'));
+	cb();
 });
 
 /**
  * Javascript Task
  */
-gulp.task('js', function(){
-	try{
-		console.log('js started');
-		gulp.src('src/js/**/*.js')
-		.pipe(plumber())
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'))
-		.pipe(browserSync.reload({stream:true}))
-		.pipe(gulp.dest('_site/assets/js/'));
-		console.log('js success!');
-	}
-	catch(e){
-		console.log('js failed');
-		console.log(e);
-	}
-	return Promise.resolve('done');
+gulp.task('js', function(cb){
+	gulp.src('src/js/**/*.js')
+	.pipe(plumber())
+	.pipe(concat('main.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('assets/js/'))
+	.pipe(browserSync.reload({stream:true}))
+	.pipe(gulp.dest('_site/assets/js/'));
+	cb();
 });
 
 /**
  * Imagemin Task
  */
-gulp.task('imagemin', function() {
-	try{
-		console.log('imagemin started');
-		gulp.src('src/img/**/*.{jpg,png,gif}')
-		.pipe(plumber())
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'));
-		console.log('imagemin failed');
-	}
-	catch(e){
-		console.log('imagemin f	ailed');
-		console.log(e);
-	}
+gulp.task('imagemin', function(cb) {
+	gulp.src('src/img/**/*.{jpg,png,gif}')
+	.pipe(plumber())
+	.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+	.pipe(gulp.dest('assets/img/'));
+	cb();
 });
 
 /**
@@ -131,25 +90,18 @@ gulp.task('imagemin', function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-	try{
-		console.log('Watch started');
-		gulp.watch('src/styl/**/*.styl', gulp.series('stylus'));
-		gulp.watch('src/js/**/*.js', gulp.series('js'));
-		gulp.watch('src/img/**/*.{jpg,png,gif}', gulp.series('imagemin'));
-		gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], gulp.series('jekyll-rebuild'));
-		console.log('Watch ended');
-	}
-	catch(e){
-		console.log('Watch failed');
-		console.log(e);
-	}
-	return Promise.resolve('done');
+	console.log('Watch started');
+	gulp.watch('src/styl/**/*.styl', gulp.series('stylus'));
+	gulp.watch('src/js/**/*.js', gulp.series('js'));
+	gulp.watch('src/img/**/*.{jpg,png,gif}', gulp.series('imagemin'));
+	gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], gulp.series('jekyll-rebuild'));
+	//console.log('Watch ended');
 });
 
 /**
  * Default task, running just `gulp` will compile the stylus,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', gulp.series('browser-sync','js', 'stylus', 'watch'),function(){
+gulp.task('default', gulp.series('js', 'stylus', gulp.parallel('browser-sync','watch'),function(){
 	return Promise.resolve('done');
-});
+}));
